e080de68f92255e0f700dd6bf274a421c7c07ffc
eliudkaruga <eliudkaruga97@gmail.com>
2024-06-06 19:58:17 +0300

    Updated import path for doctorController in allDoctors.js

--- Server/routes/allDoctors.js
+++ Server/routes/allDoctors.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {getAllDoctors} = require('../../controllers/doctorController');
+const {getAllDoctors} = require('../controllers/doctorController');
 
 router.get('/', getAllDoctors)
 
