7127dcbe6df3228811829eb142d94893e509ef0d
eliudkaruga <eliudkaruga97@gmail.com>
2024-08-15 17:09:30 +0300

    build version

--- Appointment_system/src/context/UserContext.jsx
+++ Appointment_system/src/context/UserContext.jsx
@@ -1,8 +1,12 @@
 import { createContext, useState, useEffect } from 'react';
 import { setupInterceptors } from '../API/api';
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
 
 const AppContext = createContext();
 
+
+// eslint-disable-next-line react/prop-types
 const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [token, setToken] = useState(null);
